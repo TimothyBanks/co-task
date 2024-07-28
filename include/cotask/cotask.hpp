e95fe6fb895478e1f3d89f64adc20efa9e96295d
Timothy Banks <timothyaaronbanks@gmail.com>
2024-07-28 02:36:33 +0000

    Working out cotask_context and exeuction_guard

---
+++ include/cotask/cotask.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cotask/awaitable.hpp>
+#include <cotask/cotask_context.hpp>
+#include <cotask/cotask_exception.hpp>
+#include <cotask/execution_guard.hpp>
+#include <cotask/operation_context.hpp>
+#include <cotask/task.hpp>
+#include <cotask/this_thread.hpp>
